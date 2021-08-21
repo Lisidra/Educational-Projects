03676e919210c963486323f0dc24970a0aec41ec
Viktor Chekin <77843295+Lisidra@users.noreply.github.com>
2021-08-22 00:46:24 +0300

    the method of transmitting information to elements on the page has been changed

--- Git-Statistics/js/get-gitinfo.js
+++ Git-Statistics/js/get-gitinfo.js
@@ -1,8 +1,46 @@
-let count = 0;
-
+// шаблонизатор обращений к элементам DOM
 function toElem(field) {
   return document.querySelector(field)
 }
+
+const output = (item) => {
+  toElem('.commits .quantity').textContent = item;
+}
+
+const calc = (item) => {
+  for (value in item) {
+    count++;
+  }
+  output(count)
+}
+
+const requestByLink = (item) => {
+  fetch(item)
+    .then(commit => commit.json())
+    .then(commit => calc(commit))
+}
+
+const linksToArr = (data) => {
+  let links = [];
+  data.forEach(item => links.push(item.commits_url.slice(0, -6)))
+  return links;
+}
+
+const viewCommits = (data) => {
+  for (let i = 0; i < data.length; i++) {
+    requestByLink(data[i])
+  }
+}
+
+// Task-2
+const getRepos = (userName) => {
+  fetch(`https://api.github.com/users/${userName}/repos`)
+    .then(resp => resp.json()) // data to JSON
+    .then(data => linksToArr(data)) // находим ссылки и добавляем в массив
+    .then(links => viewCommits(links)) // делаем запросы по ссылкам
+}
+
+// Основная функция
 const getInfo = (userName) => {
   fetch(`https://api.github.com/users/${userName}`)
     .then(resp => { return resp.json() })
@@ -14,37 +52,9 @@ const getInfo = (userName) => {
       toElem('.user-location').textContent = data.location; // Местоположение
       toElem('.repositories .quantity').textContent = data.public_repos; // количество публичных репозиториев
       toElem('.followers .quantity').textContent = data.followers; // количество подписчиков
+      getRepos(userName); // количество коммитов
     })
 }
 
-getInfo('lisidra');
-
-fetch(`https://api.github.com/users/${userName}/repos`)
-  // получаем массив с инфой по всем открытым репозиториям пользователя
-  // преобразовываем в json
-  .then(resp => { return resp.json() })
-  // вытаскиваем ссылки на репозитории и пушим их в массив
-  .then(data => {
-    let links = [];
-    data.forEach(repos => {
-      links.push(repos.commits_url.slice(0, -6));
-    });
-    return links;
-  })
-  // перебираем массив со ссылками
-  .then(links => {
-    for (let i = 0; i < links.length; i++) {
-      fetch(links[i])
-        .then(commits => { return commits.json() })
-        .then(commits => {
-          console.log(commits); // Выводит в консоль списки коммитов по каждому репозиторию
-          for (val in commits) {
-            count++;
-          }
-          return count
-        })
-        .then(countCommits => {
-          document.querySelector('.commits .quantity').textContent = countCommits;
-        })
-    }
-  })
+let count = 0; // счётчик коммитов
+getInfo('vetlee')
