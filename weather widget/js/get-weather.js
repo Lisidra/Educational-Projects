bf64f6cf35365ec49c61278a13b8d97d3f971ea4
Viktor Chekin <77843295+Lisidra@users.noreply.github.com>
2021-08-22 00:16:13 +0300

    the method of transmitting information to elements on the page has been changed
    * the method of transmitting information to elements on the page has been changed
    * added variables for quick change of query parameters

--- weather widget/js/get-weather.js
+++ weather widget/js/get-weather.js
@@ -1,26 +1,33 @@
-fetch('http://api.openweathermap.org/data/2.5/weather?id=498817&appid=02f63cdb5b27a3d3e7ce698be1f9c1b4&units=metric&lang=Ru')
+const apiId = '02f63cdb5b27a3d3e7ce698be1f9c1b4';
+const iconSize = 100;
+const lang = 'Ru';
+const units = 'metric';
+const cityID = '498817'
+
+function toElem(field) {
+  return document.querySelector(field)
+}
+
+fetch(`http://api.openweathermap.org/data/2.5/weather?id=${cityID}&appid=${apiId}&units=${units}&lang=${lang}`)
   .then((resp) => {
-    return resp.json() // Конвертируем data в json
+    return resp.json() // data to JSON
   })
   .then((data) => {
     console.log(data)
     document.title = `Погода - ${data.name}`
-    document.querySelector('.city-name').textContent = data.name;
-    document.querySelector('.temperature span').innerHTML = Math.round(data.main.temp) + '&#8451;';
+    toElem('.city-name').textContent = data.name;
+    toElem('.temperature span').innerHTML = Math.round(data.main.temp) + '&#8451;';
 
     // weather icon
     let icon = new Image(160, 160);
     icon.src = `http://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png`;
-    icon.width = 100;
-    icon.height = 100;
-    document.querySelector('.weather-icon').appendChild(icon);
+    icon.width = iconSize;
+    icon.height = iconSize;
+    toElem('.weather-icon').appendChild(icon);
 
-    document.querySelector('.condition').textContent = data.weather[0]['description'];
-    document.querySelector('.feels-like').innerHTML = `Ощущается как: ${Math.round(data.main.feels_like)}&#8451;`
-    document.querySelector('.wind-speed span').textContent = data.wind.speed + 'м/с';
-    document.querySelector('.atmospheric-pressure span').textContent = data.main.pressure + ' мм рт.ст.'
-    document.querySelector('.air-humidity span').textContent = data.main.humidity + '%'
-  })
-  .catch(() => {
-    // ошибки
+    toElem('.condition').textContent = data.weather[0]['description'];
+    toElem('.feels-like').innerHTML = `Ощущается как: ${Math.round(data.main.feels_like)}&#8451;`
+    toElem('.wind-speed span').textContent = data.wind.speed + 'м/с';
+    toElem('.atmospheric-pressure span').textContent = data.main.pressure + ' мм рт.ст.'
+    toElem('.air-humidity span').textContent = data.main.humidity + '%'
   })
